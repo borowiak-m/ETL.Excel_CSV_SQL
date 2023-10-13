6dd4721af724bf7fb8cb1c56fc5d0496b439bcf7
Mariusz Borowiak <neutron.meme@proton.me>
2023-10-14 00:37:41 +0100

    Looping through sheets to export

--- ExcelImport.ps1
+++ ExcelImport.ps1
@@ -18,38 +18,48 @@
 # Import the ImportExcel module
 Import-Module -Name ImportExcel
 
-
 # Initialize settings from file, where all business folder paths are stored
+$settingFileName = "ExcelImport_settings.txt"
+$settingsFolderPath = "D:\Scripts\"
+$settingsFilePath = Join-Path -Path $settingsFolderPath -ChildPath ($settingFileName)
 
-$settingsFilePath = "D:\Scripts\ExcelImport_settings.txt"
+# Check for existence of the settings file
+If (-Not(Test-Path $settingsFilePath)) {Throw "No settings document found"}
 
-if (Test-Path $settingsFilePath) {
-    $settings = @{}
+$settings = @{}
 
-    # Get variablename=filepath strings per each line, split by "=" and store in $settings dict
-
-    Get-Content $settingsFilePath | ForEach-Object {
-    $splitValue = $_ -split "="
-    $settings[$splitValue[0]] = $splitValue[1]
+# Get variablename=filepath strings per each line, split by "=" and store in $settings dict
+Get-Content $settingsFilePath | ForEach-Object {
+    $paramValuePair = $_ -split "="
+    $settings[$paramValuePair[0]] = $paramValuePair[1]
 }
-} else {
-    Throw "Missing settings file in $settingsFilePath"
-}
-
 
 # Read settings into variables
 
-$excelFilePath          = $settings['excelFilePath']
-$lastTimeFilePath       = $settings['lastTimeFilePath']
-$csvExportFilePath      = $settings['csvExportFilePath']
-$csvErrorFolderPath     = $settings['csvErrorFolderPath']
+$excelFilePath           = $settings['excelFilePath']
+$lastTimeFilePath        = $settings['lastTimeFilePath']
+$csvExportFolderPath     = $settings['csvExportFolderPath']
+$csvErrorFolderPath      = $settings['csvErrorFolderPath']
 
+# Check for the existence of excel document
+If (-Not(Test-Path $excelFilePath)) {Throw "No Excel document found"}
+
+# Check if necessary folders exist, if not create them
+$foldersToCheck = @($csvExportFolderPath, $csvErrorFolderPath)
+
+ForEach ($folder in $foldersToCheck) {
+    If (-Not (Test-Path $folder)){
+        New-Item -Path $folder -ItemType Directory
+    }
+}
+
+# Fetch last edit time of the excel document
 $lastModifiedTime = (Get-Item $excelFilePath).LastWriteTime
 
-Write-Host "Debug: excelFilePath = $excelFilePath"          #Print in command line
-Write-Host "Debug: lastModifiedTime = $lastModifiedTime"    #Print in command line
-Write-Host "Debug: csvExportFilePath  = $csvExportFilePath "    #Print in command line
-Write-Host "Debug: csvErrorFolderPath  = $csvErrorFolderPath "    #Print in command line
+Write-Host "Debug: excelFilePath        = $excelFilePath        "    #Print in command line
+Write-Host "Debug: lastModifiedTime     = $lastModifiedTime     "    #Print in command line
+Write-Host "Debug: csvExportFolderPath  = $csvExportFolderPath  "    #Print in command line
+Write-Host "Debug: csvErrorFolderPath   = $csvErrorFolderPath   "    #Print in command line
 
 if (Test-Path $lastTimeFilePath) {
 
@@ -69,15 +79,55 @@ if ($lastModifiedTime -gt $lastKnownTime) {
 
     # File has been modified since last check
     Write-Host "Debug: File modified since $lastKnownTime."
+
+    $sheetsToExport =@("TestImport1", "TestImport2")
+
+    $sheets = Get-ExcelSheetInfo $csvExportFilePath.FullName | Select -ExpandProperty Name
+
+    ForEach ($sheet in $sheets) {
+
+        ForEach ($sheetNameToExport in $sheetsToExport) {
+
+            If ($sheet.Name = $sheetNameToExport) {
+                Write-Host "Debug: Processed sheet name: $sheet.Name"
+            }
+        }
+
+        # Build file path for csv file export with sheet name
+        $csvExportFilePath = Join-Path -Path $csvExportFolderPath -ChildPath ("$sheetName.csv")
+
+        # Read from excel file
+        $allData = Import-Excel -Path $excelFilePath 
+
+        # Check if an export csv file already exists (if so move it to Error folder and replace it)
+        If (Test-Path $csvExportFilePath) {
+
+            # Generate a timestamp for error file name
+            $timestamp = Get-Date -format "yyyy.MM.dd hh.mm.ss"
+
+            # Move existing file to error folder and rename it with timestamp
+            $csvErrorFilePath = Join-Path -Path $csvErrorFolderPath -ChildPath ("Unprocessed $sheetName $timestamp.csv")
+
+            Move-Item -Path $csvExportFilePath -Destination $csvErrorFilePath
+
+        } 
+
+        #Export to csv
+        $allData | Export-Csv -Path $csvExportFilePath -NoTypeInformation -Encoding UTF8
+
+    }
+
+
+
+    ### --------- 
+
+    # After all tabs are processed, update the last modification date in the text file for next run
     Set-Content $lastTimeFilePath $lastModifiedTime.Ticks
 
-    # Read from excel file
-    $allData = Import-Excel -Path $excelFilePath -WorksheetName 'TestImport'
-
-    #Export to csv
-    $allData | Export-Csv -Path $csvExportFilePath -NoTypeInformation -Encoding UTF8
 } else {
 
     Write-Host "Debug: No new changes were written to the file"
 
 }
+
+
