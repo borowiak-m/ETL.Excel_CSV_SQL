281b96e48d59a574cf8a47c8bb2754bdd2a7b048
Mariusz Borowiak <neutron.meme@proton.me>
2023-10-17 21:54:44 +0100

    Logic explained section updated

--- CSVtoSQLimport.ps1
+++ CSVtoSQLimport.ps1
@@ -1,3 +1,25 @@
+#  LOGIC EXPLAINED
+# ---------------
+# This script aims to import data from csv files into SQL tables. How this is done is as follows:
+#
+# - Script monitors for [fileName]_settings.txt files in a folder. Each *_settings file is a collection of instructions
+#   on how to import and what to import. The fileName prefix in the file name correspond to a fileName.csv file to be
+#   processed. Its settings file will contain all fields that we are importing, and also the same fild names we will
+#   be updating in the database table specified in this settings file.
+#
+# - We iterate over this collection of files, gather a list of files to be imported and process each csv one by one, 
+#   updating their corresponding database tables.
+#
+# - Any new files will need a table created with same field names as in the csv file and a settings file created and dropped in 
+#   the monitored folder. 
+#
+# - There will be two types of data imports. And append and an overwrite, where some tables collect ongoing business data,
+#   where others will be config updates which will overwrite an entire table. These will be small in size. The indicating
+#   flag on how to update data will also be held in a settings file, particular to its import.
+#
+#
+
+
 # Define csv file to import
 $csvFolderPath                  = "D:\Scripts\Import\"
 $csvFileName                    = "CSVimport.csv"
@@ -29,14 +51,14 @@ ForEach ($row in $csvData) {
         #assign $row.$field to a viariable for each field
         # -----
         $upsertQuery = @"
-IF EXISTS (SELECT 1 FROM table_name WHERE PK_field = $PK_field)
+IF EXISTS (SELECT 1 FROM table_name WHERE ProductDepot = $ProductDepot)
     UPDATE table_name 
     SET
-    field = $field
-    WHERE PK_field = $PK_field
+    Commentary = $Commentary
+    WHERE ProductDepot = $ProductDepot
 ELSE
-    INSERT INTO table_name (field1, field2)
-    VALUES ($field1, $field2)
+    INSERT INTO table_name (ProductDepot, Commentary)
+    VALUES ( $ProductDepot, $Commentary)
 "@
         $command = New-Object System.Data.SqlClient.SqlCommand($upsertQuery, $connection)
         $command.ExecuteNonQuery()
